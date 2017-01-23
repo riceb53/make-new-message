c4bc6b1f1cf6301c72d49ac22c8b97840b0450c8
Brian <riceb53@gmail.com>
2017-01-23 14:09:40 -0500

    Fix quotes and other bugs. also install bcrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: 1)
+  end
+  helper_method :current_user
+
+  def authenticate_user
+    redirect_to '/login' unless current_user
+  end
+
+
 end
