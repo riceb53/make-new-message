c28c2f97c1818d272b4e42d53f435e0ea1456abb
Brian <riceb53@gmail.com>
2017-01-23 14:11:21 -0500

    Fix a few more bugs and get messaging system working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find_by(id: 1)
+    @current_user ||= User.find_by(id: 2)
   end
   helper_method :current_user
 
